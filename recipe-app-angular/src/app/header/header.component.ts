ad405e1f2e963544258122b3f70322348851e289
Brittany Chiu <brittanychiu@Brittanys-MacBook-Pro.local>
2020-11-16 20:35:20 -0500

    added components

---
+++ recipe-app-angular/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+// Angular imports
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['header.component.css']
+})
+
+export class  HeaderComponent {
+
+}
